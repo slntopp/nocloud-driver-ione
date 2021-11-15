d0cd220ae289aa06ddc9f682251aa84fae426049
Mik <info@slnt-opp.xyz>
2021-11-15 19:44:56 +0100

    Deploy InstancesGroups base

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -16,12 +16,21 @@ limitations under the License.
 package server
 
 import (
+	"bytes"
 	"context"
+	"crypto/sha256"
+	"encoding/base64"
+	"time"
 
+	"github.com/gofrs/uuid"
+	ione "github.com/slntopp/nocloud-driver-ione/pkg/driver"
 	pb "github.com/slntopp/nocloud/pkg/drivers/instance/vanilla"
 	instpb "github.com/slntopp/nocloud/pkg/instances/proto"
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/structpb"
 )
 
 var DRIVER_TYPE string
@@ -61,7 +70,48 @@ func (s *DriverServiceServer) ValidateConfigSyntax(ctx context.Context, request 
 	return &instpb.ValidateInstancesGroupConfigResponse{Result: true}, nil
 }
 
-func (s *DriverServiceServer) Deploy(ctx context.Context, service *instpb.InstancesGroup) (*instpb.InstancesGroup, error) {
-	s.log.Debug("Deploy request received", zap.Any("service", service))
-	return service, nil
+func (s *DriverServiceServer) Deploy(ctx context.Context, input *pb.DeployRequest) (*pb.DeployResponse, error) {
+	igroup := input.GetGroup()
+	sp := input.GetServicesProvider()
+	s.log.Debug("Deploy request received", zap.Any("instances_group", igroup))
+	
+	if igroup.GetType() != DRIVER_TYPE {
+		return nil, status.Error(codes.InvalidArgument, "Wrong driver type")
+	}
+
+	id, err := uuid.NewV4()
+	if err != nil {
+		return nil, status.Error(codes.Internal, "Couldn't generate UUID")
+	}
+
+	DATA := make(map[string]*structpb.Value, 2)
+	DATA["username"] = structpb.NewStringValue(id.String())
+
+	hasher := sha256.New()
+    hasher.Write([]byte(id.String() + time.Now().String()))
+    userPass := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
+	
+	secrets, err := MakeConfiguration(sp.GetSecrets())
+	if err != nil {
+		s.log.Error("Can't read config", zap.String("config", sp.GetSecrets()), zap.Error(err))
+		return nil, status.Error(codes.InvalidArgument, "Can't read config")
+	}
+	host := secrets.GetString("host")
+	cred := secrets.GetString("cred")
+	group := secrets.GetInt64("group_id")
+
+	client := ione.NewIONeClient(host, cred)
+	oneID, err := client.UserCreate(id.String(), userPass, group)
+	if err != nil {
+		s.log.Debug("Couldn't create OpenNebula user",
+			zap.Error(err), zap.String("login", id.String()),
+			zap.String("pass", userPass), zap.Int64("group", group) )
+		return nil, status.Error(codes.Internal, "Couldn't create OpenNebula user")
+	}
+
+	DATA["user_id"] = structpb.NewNumberValue(float64(oneID))
+
+	return &pb.DeployResponse{
+		Group: igroup, GroupData: DATA,
+	}, nil
 }
