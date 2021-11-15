832a3bdc6fa4aa3b2bfaa986a318cfdda3f6a109
Mik <info@slnt-opp.xyz>
2021-11-15 14:15:52 +0100

    test(ping) command base

--- cmd/ione/cmd/root.go
+++ cmd/ione/cmd/root.go
@@ -55,10 +55,9 @@ func init() {
 	// will be global for your application.
 
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.ione.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().String("hostname", "", "IONe Host")
+	rootCmd.PersistentFlags().StringP("username", "u", "", "Username for IONe")
+	rootCmd.PersistentFlags().StringP("password", "p", "", "Password or Token for IONe")
 }
 
 // initConfig reads in config file and ENV variables if set.
