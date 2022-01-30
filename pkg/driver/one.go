f484bdb878c07eb0f691600057835a5cbe51db16
Mik <info@slnt-opp.xyz>
2022-01-30 16:09:36 +0100

    OpenNebula Client factory from ServicesProvider proto.Message

--- pkg/driver/one.go
+++ pkg/driver/one.go
@@ -26,3 +26,19 @@ func NewClient(user, password, endpoint string, log *zap.Logger) *ONeClient {
 		log: log.Named("ONeClient"),
 	}
 }
+
+func NewClientFromSP(sp *sppb.ServicesProvider, log *zap.Logger) (*ONeClient, error) {
+	secrets := sp.GetSecrets()
+	host  := secrets["host"].GetStringValue()
+	cred  := secrets["cred"].GetStringValue()
+	if host == "" || cred == "" {
+		return nil, errors.New("Host or Credentials are empty")
+	}
+	var user, pass string
+	{
+		cred := strings.Split(cred, ":")
+		user = cred[0]
+		pass = cred[1]
+	}
+	return NewClient(host, user, pass, log), nil
+}
