77d1feeb002e469b83ea141f3de29f7221233d4b
Mik <info@slnt-opp.xyz>
2022-01-30 16:14:31 +0100

    Common pool element methods: chmod, chown

---
+++ pkg/driver/pool_element.go
@@ -0,0 +1,24 @@
+package one
+
+import (
+	"fmt"
+
+	"github.com/OpenNebula/one/src/oca/go/src/goca/schemas/shared"
+)
+
+// Chown common method
+// 	class - shorthand for entity type, possible values:
+// 		vm, vn, template, image, datastore
+func (c *ONeClient) Chown(class string, oid, uid, gid int) error {
+	_, err := c.Client.Call(fmt.Sprintf("one.%s.chown", class), oid, uid, gid)
+	return err
+}
+
+// Chmod common method
+// 	class - shorthand for entity type, possible values:
+// 		vm, vn, template, image, datastore
+func (c *ONeClient) Chmod(class string, oid, perm *shared.Permissions) error {
+	args := append([]interface{}{oid}, perm.ToArgs()...)
+	_, err := c.Client.Call(fmt.Sprintf("one.%s.chmod", class), args...)
+	return err
+}
