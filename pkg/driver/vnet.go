12aecfebba6e3cc5a857640ac7ebef3c1bb646d8
Mik <info@slnt-opp.xyz>
2022-03-11 09:23:42 +0100

    fix: typo

--- pkg/driver/vnet.go
+++ pkg/driver/vnet.go
@@ -47,7 +47,7 @@ func (c *ONeClient) ReservePublicIP(u, n int) (pool_id int, err error) {
 		user_pub_net_id = -1
 	}
 	for i := 0; i < n; i++ {
-		user_pub_net_id, err = c.ReseveVNet(
+		user_pub_net_id, err = c.ReserveVNet(
 			public_pool.ID, 1, user_pub_net_id,
 			fmt.Sprintf(USER_PUBLIC_VNET_NAME_PATTERN, u))
 		if err != nil {
@@ -90,7 +90,7 @@ func(c *ONeClient) UpdateVNet(id int, tmpl string, uType parameters.UpdateType) 
 // 	size - amount of addresses to reserve
 // 	to - VNet ID to reserve to, if set to -1 new will be created
 // 	name - name of the new VNet, if set to "", either existing will be used or new - generated
-func (c *ONeClient) ReseveVNet(id, size, to int, name string) (int, error) {
+func (c *ONeClient) ReserveVNet(id, size, to int, name string) (int, error) {
 	vnc := c.ctrl.VirtualNetwork(id)
 	tmpl := fmt.Sprintf("SIZE=%d\n", size)
 	if name != "" {
