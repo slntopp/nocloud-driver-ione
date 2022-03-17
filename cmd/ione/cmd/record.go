29b0a08dd219748259c9566adecd3c7d66b13d40
Mik <info@slnt-opp.xyz>
2022-03-17 15:40:58 +0100

    post record hook action base

---
+++ cmd/ione/cmd/record.go
@@ -0,0 +1,84 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"encoding/base64"
+	"encoding/xml"
+
+	pb "github.com/slntopp/nocloud-driver-ione/pkg/edge/proto"
+	"google.golang.org/protobuf/types/known/structpb"
+
+	"github.com/OpenNebula/one/src/oca/go/src/goca/schemas/vm"
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+)
+
+// recordCmd represents the record command
+var recordCmd = &cobra.Command{
+	Use:   "record",
+	Short: "Sends VM State to NoCloud",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		encoded := args[0]
+		tmpl, err := base64.StdEncoding.DecodeString(encoded)
+		if err != nil {
+			return err
+		}
+		vm := &vm.VM{}
+		err = xml.Unmarshal(tmpl, vm)
+		if err != nil {
+			return err
+		}
+		
+		st, lcm_st, err := vm.State()
+		if err != nil {
+			return err
+		}
+
+		state := int(st)
+		state_str := st.String()
+		lcm_state := int(lcm_st)
+		lcm_state_str := lcm_st.String()
+
+		log.Info("Storing Instance State", 
+			zap.Int("vmid", vm.ID), zap.String("instance", vm.Name),
+			zap.Int("state", state), zap.String("state_str", state_str),
+			zap.Int("lcm_state", lcm_state), zap.String("lcm_state_str", lcm_state_str),
+		)
+
+		data, err := structpb.NewStruct(map[string]interface{}{
+			"uuid":          vm.Name,
+			"state":         state,
+			"state_str":     state_str,
+			"lcm_state":     lcm_state,
+			"lcm_state_str": lcm_state_str,
+		})
+		if err != nil {
+			return err
+		}
+
+		edgeClient.PostInstanceState(ctx, &pb.InstanceState{
+			Instance: vm.Name,
+			Data: data.GetFields(),
+		})
+
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(recordCmd)
+}
--- cmd/ione/cmd/root.go
+++ cmd/ione/cmd/root.go
@@ -16,24 +16,27 @@ limitations under the License.
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"os"
 
-	ione "github.com/slntopp/nocloud-driver-ione/pkg/driver"
 	one "github.com/slntopp/nocloud-driver-ione/pkg/driver"
+	pb "github.com/slntopp/nocloud-driver-ione/pkg/edge/proto"
 	"github.com/slntopp/nocloud/pkg/nocloud"
 	"github.com/spf13/cobra"
 	"go.uber.org/zap"
+	"google.golang.org/grpc"
 
 	"github.com/spf13/viper"
 )
 
 var (
-	cfgFile string
-	host, user, pass string
 	client *one.ONeClient
 
 	log *zap.Logger
+
+	ctx context.Context
+	edgeClient pb.EdgeServiceClient
 )
 	
 // rootCmd represents the base command when called without any subcommands
@@ -46,9 +49,6 @@ examples and usage of using your application. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -60,32 +60,13 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 	log = nocloud.NewLogger()
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.ione.yaml)")
-	rootCmd.PersistentFlags().StringVar(&host, "hostname", "", "IONe Host")
-	rootCmd.PersistentFlags().StringVarP(&user, "username", "u", "", "Username for IONe")
-	rootCmd.PersistentFlags().StringVarP(&pass, "password", "p", "", "Password or Token for IONe")
 }
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
-
-		// Search config in home directory with name ".ione" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName(".ione")
-	}
+	viper.AddConfigPath("/etc/one")
+	viper.SetConfigType("yaml")
+	viper.SetConfigName("ione")
 
 	viper.AutomaticEnv() // read in environment variables that match
 
@@ -94,8 +75,11 @@ func initConfig() {
 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
 	}
 
-	if host == "" || user == "" || pass == "" {
-		panic("Hostname, Username or Password not given")
+	conn, err := grpc.Dial("localhost:8080")
+	if err != nil {
+		panic(err)
 	}
-	client = ione.NewClient(user, pass, host, log)
+
+	ctx = context.Background()
+	edgeClient = pb.NewEdgeServiceClient(conn)
 }
