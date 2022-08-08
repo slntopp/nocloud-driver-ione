c2db19b531a0ce6199e5479530ce42e73d2e622e
Mik Iwanowski <info@slnt-opp.xyz>
2022-08-08 16:53:46 +0200

    moved to cli

--- cmd/vnc/server.go
+++ cmd/vnc/server.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"encoding/base64"
 	"net/http"
-	"os"
 
-	"github.com/OpenNebula/one/src/oca/go/src/goca"
 	"github.com/slntopp/nocloud/pkg/nocloud"
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
@@ -32,24 +29,6 @@ func init() {
 }
 
 func main() {
-	if len(os.Args) >= 3 {
-		if os.Args[1] != "test" {
-			log.Fatal("Unrecognized argument", zap.String("arg", os.Args[1]))
-		}
-		kind := "vnc"
-		if len(os.Args) == 4 {
-			kind = os.Args[3]
-		}
-		if kind != "vnc" && kind != "vmrc" {
-			log.Fatal("Unsupported kind", zap.String("kind", kind))
-		}
-		c := goca.NewController(goca.NewDefaultClient(goca.NewConfig("oneadmin", "72cf91142b4593541993b02b9bfc22a9d4666afd09f95b1f268f4204dee3b1c4", "https://sunstone.demo.support.pl/RPC2")))
-		d := GenToken(c, os.Args[2], kind)
-		ui := base64.StdEncoding.EncodeToString([]byte(d.Url))
-		log.Info("Resulting URL", zap.String("url", d.Url), zap.String("ui", "https://sunstone.demo.support.pl/vnc?socket="+ui))
-		return
-	}
-
 	http.HandleFunc("/", HandleGenerateToken)
 	http.ListenAndServe(":8010", nil)
 }
