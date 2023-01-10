63cbce69eaf15e90f5dff072fc1a191a45aa4c56
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2023-01-10 22:39:46 +0300

    Complete base test

--- pkg/driver/vm_test.go
+++ pkg/driver/vm_test.go
@@ -1,7 +1,42 @@
 package one
 
-import "testing"
+import (
+	"github.com/OpenNebula/one/src/oca/go/src/goca"
+	stpb "github.com/slntopp/nocloud-proto/states"
+	"testing"
+)
 
 func TestFilterTimeline(t *testing.T) {
+	conf := goca.NewConfig("user", "pass", "host")
+	c := goca.NewClient(conf, nil)
+	ctrl := goca.NewController(c)
 
+	machine, err := ctrl.VM(6165).Info(true)
+
+	if err != nil {
+		t.Errorf("Cannot get vm")
+	}
+
+	timeline := MakeTimeline(machine)
+
+	expectedLen := 4
+
+	if len(timeline) != expectedLen {
+		t.Errorf("Wrong num of records")
+	}
+
+	filtered := FilterTimeline(timeline, 1673376000, 1673378500)
+	expectedLenFiltered := 2
+
+	if len(filtered) != expectedLenFiltered {
+		t.Errorf("Wrong num of filtered records")
+	}
+
+	if filtered[0].State != stpb.NoCloudState_SUSPENDED {
+		t.Errorf("Wrong state of record")
+	}
+
+	if filtered[len(filtered)-1].State != stpb.NoCloudState_RUNNING {
+		t.Errorf("Wrong state of record")
+	}
 }
