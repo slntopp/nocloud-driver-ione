40a3f60d89bf0343e97920b5be06254cfbcfae7e
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2023-01-31 14:06:15 +0300

    Fix setup

--- main.go
+++ main.go
@@ -108,19 +108,17 @@ func main() {
 }
 
 func SetupRecordsPublisher(rbmq *amqp.Connection) server.RecordsPublisherFunc {
+	ch, err := rbmq.Channel()
+	if err != nil {
+		log.Fatal("Failed to open a channel", zap.Error(err))
+	}
+
+	queue, _ := ch.QueueDeclare(
+		"records",
+		true, false, false, true, nil,
+	)
+
 	return func(ctx context.Context, payload []*billingpb.Record) error {
-		ch, err := rbmq.Channel()
-		if err != nil {
-			log.Error("Failed to open a channel", zap.Error(err))
-			return err
-		}
-		defer ch.Close()
-
-		queue, _ := ch.QueueDeclare(
-			"records",
-			true, false, false, true, nil,
-		)
-
 		for _, record := range payload {
 			body, err := proto.Marshal(record)
 			if err != nil {
