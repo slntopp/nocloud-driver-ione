cf577c8af3eaee3c87b4f2ab395ffdaffa88523f
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2023-12-27 14:31:03 +0300

    Fix auto_start

--- pkg/driver/vm.go
+++ pkg/driver/vm.go
@@ -469,7 +469,21 @@ func (c *ONeClient) CheckInstancesGroup(IG *pb.InstancesGroup) (*CheckInstancesG
 		vm, err := c.FindVMByInstance(inst)
 		if err != nil {
 			log.Warn("VM is still not created", zap.Error(err), zap.Any("inst", inst))
-			resp.ToBeCreated = append(resp.ToBeCreated, inst)
+			meta := inst.GetBillingPlan().GetMeta()
+			if meta == nil {
+				meta = make(map[string]*structpb.Value)
+			}
+			cfg := inst.GetConfig()
+			if cfg == nil {
+				cfg = make(map[string]*structpb.Value)
+			}
+
+			cfgAutoStart := cfg["auto_start"].GetBoolValue()
+			metaAutoStart := meta["auto_start"].GetBoolValue()
+
+			if metaAutoStart || cfgAutoStart {
+				resp.ToBeCreated = append(resp.ToBeCreated, inst)
+			}
 			continue
 		}
 
