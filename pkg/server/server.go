04a26661237bdf51442880326ecb334d1fb5e236
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2024-01-02 13:52:45 +0300

    Fix

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -441,14 +441,14 @@ func (s *DriverServiceServer) Monitoring(ctx context.Context, req *pb.Monitoring
 			cfgAutoStart := cfg["auto_start"].GetBoolValue()
 			metaAutoStart := meta["auto_start"].GetBoolValue()
 
-			if !(metaAutoStart || cfgAutoStart) {
-				instStatePublisher := datas.StatePublisher(datas.POST_INST_STATE)
-				instStatePublisher(inst.GetUuid(), &stpb.State{State: stpb.NoCloudState_PENDING, Meta: map[string]*structpb.Value{}})
-			} else if inst.GetStatus() != statuspb.NoCloudStatus_DEL {
+			if inst.GetStatus() != statuspb.NoCloudStatus_DEL {
 				_, err = actions.StatusesClient(client, inst, inst.GetData(), &ipb.InvokeResponse{Result: true})
 				if err != nil {
 					log.Error("Error Monitoring Instance", zap.Any("instance", inst), zap.Error(err))
 				}
+			} else if !(metaAutoStart || cfgAutoStart) {
+				instStatePublisher := datas.StatePublisher(datas.POST_INST_STATE)
+				instStatePublisher(inst.GetUuid(), &stpb.State{State: stpb.NoCloudState_PENDING, Meta: map[string]*structpb.Value{}})
 			} else {
 				instStatePublisher := datas.StatePublisher(datas.POST_INST_STATE)
 				instStatePublisher(inst.GetUuid(), &stpb.State{State: stpb.NoCloudState_DELETED})
