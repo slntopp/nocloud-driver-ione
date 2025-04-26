b7b382701c90422e77e2843790615fcef48c00d3
coddmeistr <euseew.maxim2015@yandex.ru>
2025-04-26 12:34:41 +0300

    do not send deleted event twice

--- pkg/server/events.go
+++ pkg/server/events.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"github.com/slntopp/nocloud-driver-ione/pkg/datas"
 	one "github.com/slntopp/nocloud-driver-ione/pkg/driver"
 	epb "github.com/slntopp/nocloud-proto/events"
 	"google.golang.org/protobuf/types/known/structpb"
@@ -9,10 +10,18 @@ import (
 
 func handleInstEvents(ctx context.Context, resp *one.CheckInstancesGroupResponse, events EventsPublisherFunc) {
 	for _, inst := range resp.ToBeDeleted {
+		if inst.Data == nil {
+			inst.Data = make(map[string]*structpb.Value)
+		}
+		if inst.Data["deleted_notification"].GetBoolValue() {
+			continue
+		}
 		go events(ctx, &epb.Event{
 			Uuid: inst.GetUuid(),
 			Key:  "instance_deleted",
 			Data: map[string]*structpb.Value{},
 		})
+		inst.Data["deleted_notification"] = structpb.NewBoolValue(true)
+		datas.DataPublisher(datas.POST_INST_DATA)(inst.GetUuid(), inst.Data)
 	}
 }
